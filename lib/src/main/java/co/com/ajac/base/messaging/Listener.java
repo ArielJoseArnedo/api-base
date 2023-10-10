3d8478913957b2a17b475cdb9d4f9f8e5f6371ee
Ariel Arnedo <ariel_arnedo@epam.com>
2023-10-10 13:31:29 -0500

    se modifica la estructura del modelo de eventos

--- lib/src/main/java/co/com/ajac/base/messaging/Listener.java
+++ lib/src/main/java/co/com/ajac/base/messaging/Listener.java
@@ -1,14 +1,14 @@
 package co.com.ajac.base.messaging;
 
-import co.com.ajac.base.events.Event;
-import co.com.ajac.base.events.Header;
-import co.com.ajac.base.events.Message;
+import co.com.ajac.base.events.IEvent;
+import co.com.ajac.base.events.IHeader;
+import co.com.ajac.base.events.IMessage;
 import io.vavr.collection.List;
 import io.vavr.concurrent.Future;
 
 public interface Listener {
     String getName();
-    default Future<List<Event>> onMessage(Message message, Header header) {
+    default Future<List<IEvent>> onMessage(IMessage IMessage, IHeader IHeader) {
         return Future.successful(List.empty());
     }
 }
--- lib/src/main/java/co/com/ajac/base/messaging/Publisher.java
+++ lib/src/main/java/co/com/ajac/base/messaging/Publisher.java
@@ -1,9 +1,9 @@
 package co.com.ajac.base.messaging;
 
-import co.com.ajac.base.events.Event;
+import co.com.ajac.base.events.IEvent;
 import io.vavr.collection.List;
 import io.vavr.concurrent.Future;
 
 public interface Publisher {
-    Future<List<Event>> publish(List<Event> events);
+    Future<List<IEvent>> publish(List<IEvent> events);
 }
