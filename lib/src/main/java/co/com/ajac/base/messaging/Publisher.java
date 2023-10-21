2b841c6071732699cff7b853dea205825e4d2f65
Ariel Arnedo <ariel_arnedo@epam.com>
2023-10-21 10:14:33 -0500

    se actualiza la version de jdk a 17

--- lib/src/main/java/co/com/ajac/base/messaging/Publisher.java
+++
@@ -1,9 +0,0 @@
-package co.com.ajac.base.messaging;
-
-import co.com.ajac.base.events.IEvent;
-import io.vavr.collection.List;
-import io.vavr.concurrent.Future;
-
-public interface Publisher {
-    Future<List<IEvent>> publish(List<IEvent> events);
-}
